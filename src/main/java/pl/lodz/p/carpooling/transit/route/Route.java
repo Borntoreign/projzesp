3aa4510aee35ab385f60766c4118f4ba35ba2c73
Adrian Szwajkowski <galowy@gmail.com>
2015-12-15 17:42:15 +0100

    Fixing bug where routes are not inserted properly when the sample import sql is present. Route ID is now generated in appropriate way. Modified sample data import.

--- src/main/java/pl/lodz/p/carpooling/transit/route/Route.java
+++ src/main/java/pl/lodz/p/carpooling/transit/route/Route.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Entity
 public class Route {
 
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private long id;
 
