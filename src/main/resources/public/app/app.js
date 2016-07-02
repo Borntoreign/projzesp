974a174c907aae3a830c0a24d41f16c2aad44632
Łukasz Wieczorek <czarny445@gmail.com>
2016-07-02 16:38:14 +0200

    Mapa

--- src/main/resources/public/app/app.js
+++ src/main/resources/public/app/app.js
@@ -2,20 +2,22 @@
 
 (function () {
     angular
-        .module('carpooling', [
-            'carpooling.auth',
-            'carpooling.transit',
-            'carpooling.search',
-            'carpooling.settings',
-            'carpooling.reservation',
-            'carpooling.user',
-            'carpooling.fileupload',
-            'ui.bootstrap',
-            'ui.router',
-            'ngCookies',
-            'uiGmapgoogle-maps',
-            'ngAnimate',
-            'ui.bootstrap.datetimepicker',
-            'ui.validate',
-        ]);
+            .module('carpooling', [
+                'ngMap',
+                'carpooling.auth',
+                'carpooling.transit',
+                'carpooling.search',
+                'carpooling.settings',
+                'carpooling.reservation',
+                'carpooling.user',
+                'carpooling.fileupload',
+                'ui.bootstrap',
+                'ui.router',
+                'ngCookies',
+                'uiGmapgoogle-maps',
+                'ngAnimate',
+                'ui.bootstrap.datetimepicker',
+                'ui.validate'
+            ]);
+
 }());
